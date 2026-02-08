0ec0b40dde4ee01804714117ae4d99efb12bfad9
ElebrUS <megaelebrus@ya.ru>
2026-02-08 18:26:03 +0200

    Add Dotenv

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
 from services import RouterService, BotService, Storage, MonitorService
 
 
 def main():
+    load_dotenv()
     router = RouterService.from_settings()
     bot = BotService.from_settings()
     storage = Storage()
